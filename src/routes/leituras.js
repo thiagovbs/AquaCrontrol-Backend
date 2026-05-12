bfa50f98be064656590706c5e30ace594099fc44
Thiago Veloso <thiago.vbs@gmail.com>
2026-05-12 18:15:37 -0300

    Update leituras.js

--- src/routes/leituras.js
+++ src/routes/leituras.js
@@ -2,7 +2,7 @@ const express = require('express');
 const prisma = require('../lib/prisma');
 const auth = require('../middleware/auth');
 const router = express.Router();
-const prisma = new PrismaClient();
+
 
 router.get('/', async (req, res) => {
   try {
