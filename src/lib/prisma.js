f900aa16e0376f43bfaf876ea9bc29db94aee669
Thiago Veloso <thiago.vbs@gmail.com>
2026-05-12 18:29:41 -0300

    Update prisma.js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('../generated/client');
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
   log: ['query', 'error', 'warn'], // Isso vai nos ajudar a ver o que o banco está fazendo
