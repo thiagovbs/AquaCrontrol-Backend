60f26427aff48a46ac2b321b4e459226d5c3ac32
Thiago Veloso <thiago.vbs@gmail.com>
2026-05-12 18:12:55 -0300

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('../lib/prisma');
+const prisma = require('../lib/prisma');
 const router = express.Router();
 const prisma = new PrismaClient();
 
