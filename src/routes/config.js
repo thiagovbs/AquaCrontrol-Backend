c9e7aa8848ae6f3ff2edb29d1be52cbf38260c23
Thiago Veloso <thiago.vbs@gmail.com>
2026-05-12 18:12:46 -0300

    Update config.js

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { PrismaClient } = require('../lib/prisma');
+const prisma = require('../lib/prisma');
 const prisma = new PrismaClient();
 
 // Busca o valor atual (Sempre pega o ID 1)
