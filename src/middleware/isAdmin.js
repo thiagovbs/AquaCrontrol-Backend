fd13580b0f30ab9bf7c4af6ffbb2d297b6954133
Thiago Veloso <thiago.vbs@gmail.com>
2026-05-12 17:06:44 -0300

    Update isAdmin.js

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
-  console.log("O QUE CHEGOU NO IS_ADMIN:", req.user);
+  const usuario = req.user.user || req.user;
   // O middleware de auth anterior já deve ter colocado o user na requisição
-  if (req.user && req.user.role === 'ADMIN') {
+  if (usuario && usuario.role === 'ADMIN') {
     next(); // É admin, pode passar
   } else {
     res.status(403).json({ error: 'Acesso negado. Requer cargo de Administrador.' });
